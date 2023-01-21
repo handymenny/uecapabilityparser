8584b469181825331874cca559bea15181464a6f
handymenny <handymenny@outlook.com>
2023-01-21 20:13:55 +0100

    FeaturePerCCNr: add to string metho

--- src/main/java/it/smartphonecombo/uecapabilityparser/bean/nr/FeaturePerCCNr.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/bean/nr/FeaturePerCCNr.kt
@@ -14,4 +14,13 @@ class FeaturePerCCNr(
         const val UPLINK = 1
         const val DOWNlINK = 0
     }
+
+    override fun toString(): String {
+        val typeString = when (type) {
+            DOWNlINK -> "DL"
+            UPLINK -> "UL"
+            else -> "Unknown"
+        }
+        return "FeaturePerCCNr(type=$typeString, mimo=$mimo, qam=$qam, bw=$bw, scs=$scs, bw90MHz=$channelBW90mhz)"
+    }
 }
