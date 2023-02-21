9eadd3b4e3a863c86d34dd311e180763dc9ff7ea
handymenny <handymenny@outlook.com>
2023-02-21 23:10:44 +0100

    drop oldEngine

--- src/main/java/it/smartphonecombo/uecapabilityparser/MainCli.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/MainCli.kt
@@ -14,14 +14,10 @@ import it.smartphonecombo.uecapabilityparser.bean.IComponent
 import it.smartphonecombo.uecapabilityparser.bean.Rat
 import it.smartphonecombo.uecapabilityparser.bean.lte.ComboLte
 import it.smartphonecombo.uecapabilityparser.importer.ImportCapabilities
-import it.smartphonecombo.uecapabilityparser.importer.Tshark
 import it.smartphonecombo.uecapabilityparser.importer.lte.Import0xB0CD
 import it.smartphonecombo.uecapabilityparser.importer.lte.ImportCarrierPolicy
 import it.smartphonecombo.uecapabilityparser.importer.lte.ImportMTKLte
 import it.smartphonecombo.uecapabilityparser.importer.lte.ImportNvItem
-import it.smartphonecombo.uecapabilityparser.importer.ltenr.ImportCellularPro
-import it.smartphonecombo.uecapabilityparser.importer.ltenr.ImportNsg
-import it.smartphonecombo.uecapabilityparser.importer.ltenr.ImportWireshark
 import it.smartphonecombo.uecapabilityparser.importer.nr.Import0xB826
 import it.smartphonecombo.uecapabilityparser.importer.nr.ImportCapPrune
 import it.smartphonecombo.uecapabilityparser.newEngine.ImportCapabilityInformationJson
@@ -101,7 +97,7 @@ internal object MainCli {
             "n",
             "newEngine",
             false,
-            "Use the new experimental engine. It works only with type H, W, N, O."
+            "Use the new engine (default)"
         )
         options.addOption(newEngine)
 
@@ -122,18 +118,13 @@ internal object MainCli {
                 flags["TsharkPath"] = cmd.getOptionValue("TsharkPath")
             }
             val typeLog = cmd.getOptionValue("type")
-            val comboList: Capabilities
-            if (cmd.hasOption("newEngine") &&
-                (typeLog == "H" || typeLog == "N" || typeLog == "W" || typeLog == "O")) {
-                comboList = newEngine(cmd, typeLog)
-            } else {
-                comboList = oldEngine(cmd, typeLog)
-            }
+            val comboList = parsing(cmd, typeLog)
 
             if (cmd.hasOption("csv")) {
                 val fileName: String? = cmd.getOptionValue("csv")
-                if (typeLog == "W" || typeLog == "N" || typeLog == "H" || typeLog == "P" || typeLog == "QNR"
-                    || typeLog == "O") {
+                if (typeLog == "W" || typeLog == "N" || typeLog == "H" || typeLog == "P" || typeLog == "QNR" ||
+                    typeLog == "O"
+                ) {
                     val lteCombos = comboList.lteCombos
                     if (!lteCombos.isNullOrEmpty()) {
                         outputFile(
@@ -196,70 +187,28 @@ internal object MainCli {
         }
     }
 
-    private fun oldEngine(
+    private fun parsing(
         cmd: CommandLine,
         typeLog: String
     ): Capabilities {
         try {
-            var input: String
-            var inputNR = ""
-            var inputENDC = ""
-            if (typeLog == "E" || typeLog == "M") {
-                input = cmd.getOptionValue("input")
+            val input = if (typeLog == "E" || typeLog == "M") {
+                cmd.getOptionValue("input")
             } else {
-                input = Utility.readFile(cmd.getOptionValue("input"), StandardCharsets.UTF_8)
-
-                if (typeLog == "H" || typeLog == "P" || typeLog == "N" || typeLog == "W") {
-                    if (cmd.hasOption("inputNR")) {
-                        inputNR =
-                            Utility.readFile(cmd.getOptionValue("inputNR"), StandardCharsets.UTF_8)
-                    }
-                    if (cmd.hasOption("inputENDC")) {
-                        inputENDC = Utility.readFile(
-                            cmd.getOptionValue("inputENDC"),
-                            StandardCharsets.UTF_8
-                        )
-                    }
-
-                    if (typeLog != "H") {
-                        input += inputENDC + inputNR
-                    }
-                }
+                Utility.readFile(cmd.getOptionValue("input"), StandardCharsets.UTF_8)
             }
             val imports: ImportCapabilities?
             when (typeLog) {
-                "W" -> imports = ImportWireshark()
-                "N" -> imports = ImportNsg()
-                "H" -> {
-                    val tshark = Tshark()
-                    val rat = if (cmd.hasOption("defaultNR")) Rat.nr else Rat.eutra
-                    input = tshark.startDecoder(input, "lte-rrc.ul.dcch", rat)
-                    if (inputENDC.isNotBlank()) {
-                        input += tshark.startDecoder(inputENDC, "lte-rrc.ul.dcch", Rat.eutra_nr)
-                    }
-                    if (inputNR.isNotBlank()) {
-                        input += tshark.startDecoder(inputNR, "lte-rrc.ul.dcch", Rat.nr)
-                    }
-                    imports = ImportWireshark()
-                }
-
-                "P" -> imports = ImportCellularPro()
                 "E" -> imports = ImportNvItem()
                 "C" -> imports = ImportCarrierPolicy()
                 "CNR" -> imports = ImportCapPrune()
                 "Q" -> imports = Import0xB0CD()
                 "M" -> imports = ImportMTKLte()
                 "QNR" -> imports = Import0xB826()
-                "O" -> {
-                    System.err.println(
-                        "Type O (Osix) is supported only by the new experimental engine.\n" +
-                                "You can enable it with --newEngine."
-                    )
-                    exitProcess(1)
-                }
+                "W", "N", "O", "H" -> return ueCapabilityHandling(cmd, typeLog)
                 else -> {
                     System.err.println(
-                        "Only type W (wireshark), N (NSG), H (Hex Dump), P (CellularPro), " +
+                        "Only type W (wireshark), N (NSG), H (Hex Dump), Osix (CellularPro), " +
                             "C (Carrier policy), CNR (NR Cap Prune), E (28874 nvitem), " +
                             "Q (0xB0CD text), M (CA_COMB_INFO), QNR (0xB826 hexdump) are supported!"
                     )
@@ -284,132 +233,141 @@ internal object MainCli {
         }
     }
 
-    private fun newEngine(
+    private fun ueCapabilityHandling(
         cmd: CommandLine,
         typeLog: String
     ): Capabilities {
-        try {
-            var input: String
-            var inputNR = ""
-            var inputENDC = ""
-            input = Utility.readFile(cmd.getOptionValue("input"), StandardCharsets.UTF_8)
-            if (typeLog == "H" || typeLog == "N" || typeLog == "W") {
-                if (cmd.hasOption("inputNR")) {
-                    inputNR =
-                        Utility.readFile(cmd.getOptionValue("inputNR"), StandardCharsets.UTF_8)
-                }
-                if (cmd.hasOption("inputENDC")) {
-                    inputENDC = Utility.readFile(
-                        cmd.getOptionValue("inputENDC"),
-                        StandardCharsets.UTF_8
-                    )
-                }
-                if (typeLog == "H") {
-                    input = preformatHexData(input)
-                    inputNR = preformatHexData(inputNR)
-                    inputENDC = preformatHexData(inputENDC)
-                } else {
-                    input += inputENDC + inputNR
-                }
+        var input: String
+        var inputNR = ""
+        var inputENDC = ""
+        input = Utility.readFile(cmd.getOptionValue("input"), StandardCharsets.UTF_8)
+        if (typeLog == "H" || typeLog == "N" || typeLog == "W") {
+            if (cmd.hasOption("inputNR")) {
+                inputNR =
+                    Utility.readFile(cmd.getOptionValue("inputNR"), StandardCharsets.UTF_8)
             }
-            val imports = ImportCapabilityInformationJson()
-            val formatWriter = KotlinJsonFormatWriter()
-            val ratContainerMap = mutableMapOf<String, JsonElement>()
-            lateinit var eutraIdentifier: Regex
-            lateinit var nrIdentifier: Regex
-            lateinit var mrdcIdentifier: Regex
-
-            val converter = when (typeLog) {
-                "W" -> {
-                    eutraIdentifier = "${Rat.eutra.ratCapabilityIdentifier}\\s".toRegex()
-                    nrIdentifier = "${Rat.nr.ratCapabilityIdentifier}\\s".toRegex()
-                    mrdcIdentifier = "${Rat.eutra_nr.ratCapabilityIdentifier}\\s".toRegex()
-                    ConverterWireshark()
-                }
-                "N" -> {
-                    eutraIdentifier = "rat-Type : ${Rat.eutra}\\s".toRegex()
-                    nrIdentifier = "rat-Type : ${Rat.nr}\\s".toRegex()
-                    mrdcIdentifier = "rat-Type : ${Rat.eutra_nr}\\s".toRegex()
-                    ConverterNSG()
-                }
-                "O" -> {
-                    eutraIdentifier = "${Rat.eutra.ratCapabilityIdentifier}\\s".toRegex()
-                    nrIdentifier = "${Rat.nr.ratCapabilityIdentifier}\\s".toRegex()
-                    mrdcIdentifier = "${Rat.eutra_nr.ratCapabilityIdentifier}\\s".toRegex()
-                    ConverterOsix()
-                }
-                "H" -> null
-                else -> {
-                    System.err.println(
-                        "Only type W (wireshark), N (NSG), H (Hex Dump), O (Osix) are supported " +
-                            "by the new Engine!"
-                    )
-                    exitProcess(1)
-                }
+            if (cmd.hasOption("inputENDC")) {
+                inputENDC = Utility.readFile(
+                    cmd.getOptionValue("inputENDC"),
+                    StandardCharsets.UTF_8
+                )
             }
-
             if (typeLog == "H") {
-                val defaultRat = if (cmd.hasOption("defaultNR")) Rat.nr else Rat.eutra
-                ratContainerMap += Utility.getUeCapabilityJsonFromHex(defaultRat, input)
-                if (inputNR.isNotBlank()) {
-                    ratContainerMap += Utility.getUeCapabilityJsonFromHex(Rat.nr, input)
-                }
-                if (inputENDC.isNotBlank()) {
-                    ratContainerMap += Utility.getUeCapabilityJsonFromHex(Rat.eutra_nr, input)
-                }
+                input = preformatHexData(input)
+                inputNR = preformatHexData(inputNR)
+                inputENDC = preformatHexData(inputENDC)
             } else {
-                val list = listOf(
-                    Rat.eutra to input.indexOf(eutraIdentifier),
-                    Rat.eutra_nr to input.indexOf(mrdcIdentifier),
-                    Rat.nr to input.indexOf(nrIdentifier)
-                ).filter { it.second != -1 }.sortedBy { it.second }
-                var eutra = ""
-                var eutraNr = ""
-                var nr = ""
-                for (i in list.indices) {
-                    val (rat, start) = list[i]
-                    val end = list.getOrNull(i + 1)?.second ?: input.length
-                    when (rat) {
-                        Rat.eutra -> eutra = input.substring(start + eutraIdentifier.toString().length - 1, end)
-                        Rat.eutra_nr -> eutraNr = input.substring(start + mrdcIdentifier.toString().length - 1, end)
-                        Rat.nr -> nr = input.substring(start + nrIdentifier.toString().length - 1, end)
-                        else -> {}
-                    }
-                }
-                if (eutra.isNotBlank()) {
-                    getAsn1Converter(Rat.eutra, converter!!)
-                        .convert(Rat.eutra.ratCapabilityIdentifier, eutra.byteInputStream(), formatWriter)
-                    formatWriter.jsonNode?.let { ratContainerMap.put(Rat.eutra.toString(), it) }
-                }
-                if (eutraNr.isNotBlank() || nr.isNotBlank()) {
-                    val nrConverter = getAsn1Converter(Rat.nr, converter!!)
-                    if (eutraNr.isNotBlank()) {
-                        nrConverter.convert(Rat.eutra_nr.ratCapabilityIdentifier, eutraNr.byteInputStream(), formatWriter)
-                        formatWriter.jsonNode?.let { ratContainerMap.put(Rat.eutra_nr.toString(), it) }
-                    }
-                    if (nr.isNotBlank()) {
-                        nrConverter.convert(Rat.nr.ratCapabilityIdentifier, nr.byteInputStream(), formatWriter)
-                        formatWriter.jsonNode?.let { ratContainerMap.put(Rat.nr.toString(), it) }
-                    }
-                }
+                input += inputENDC + inputNR
             }
-
-            val jsonOutput = JsonObject(ratContainerMap)
-
-            if (cmd.hasOption("uelog")) {
-                val outputFile = cmd.getOptionValue("uelog")
-                outputFile(jsonOutput.toString(), outputFile)
-            }
-
-            val jsonEutra = jsonOutput.getOrDefault(Rat.eutra.toString(), null) as? JsonObject
-            val jsonEutraNr = jsonOutput.getOrDefault(Rat.eutra_nr.toString(), null) as? JsonObject
-            val jsonNr = jsonOutput.getOrDefault(Rat.nr.toString(), null) as? JsonObject
-
-            return imports.parse(jsonEutra, jsonEutraNr, jsonNr)
-        } catch (e: Exception) {
-            System.err.println("Error")
-            e.printStackTrace()
-            exitProcess(1)
         }
+        val imports = ImportCapabilityInformationJson()
+        val formatWriter = KotlinJsonFormatWriter()
+        val ratContainerMap = mutableMapOf<String, JsonElement>()
+        lateinit var eutraIdentifier: Regex
+        lateinit var nrIdentifier: Regex
+        lateinit var mrdcIdentifier: Regex
+
+        val converter = when (typeLog) {
+            "W" -> {
+                eutraIdentifier = "${Rat.eutra.ratCapabilityIdentifier}\\s".toRegex()
+                nrIdentifier = "${Rat.nr.ratCapabilityIdentifier}\\s".toRegex()
+                mrdcIdentifier = "${Rat.eutra_nr.ratCapabilityIdentifier}\\s".toRegex()
+                ConverterWireshark()
+            }
+
+            "N" -> {
+                eutraIdentifier = "rat-Type : ${Rat.eutra}\\s".toRegex()
+                nrIdentifier = "rat-Type : ${Rat.nr}\\s".toRegex()
+                mrdcIdentifier = "rat-Type : ${Rat.eutra_nr}\\s".toRegex()
+                ConverterNSG()
+            }
+
+            "O" -> {
+                eutraIdentifier = "${Rat.eutra.ratCapabilityIdentifier}\\s".toRegex()
+                nrIdentifier = "${Rat.nr.ratCapabilityIdentifier}\\s".toRegex()
+                mrdcIdentifier = "${Rat.eutra_nr.ratCapabilityIdentifier}\\s".toRegex()
+                ConverterOsix()
+            }
+
+            "H" -> null
+            else -> null
+        }
+
+        if (typeLog == "H") {
+            val defaultRat = if (cmd.hasOption("defaultNR")) Rat.nr else Rat.eutra
+            ratContainerMap += Utility.getUeCapabilityJsonFromHex(defaultRat, input)
+            if (inputNR.isNotBlank()) {
+                ratContainerMap += Utility.getUeCapabilityJsonFromHex(Rat.nr, input)
+            }
+            if (inputENDC.isNotBlank()) {
+                ratContainerMap += Utility.getUeCapabilityJsonFromHex(Rat.eutra_nr, input)
+            }
+        } else {
+            val list = listOf(
+                Rat.eutra to input.indexOf(eutraIdentifier),
+                Rat.eutra_nr to input.indexOf(mrdcIdentifier),
+                Rat.nr to input.indexOf(nrIdentifier)
+            ).filter { it.second != -1 }.sortedBy { it.second }
+            var eutra = ""
+            var eutraNr = ""
+            var nr = ""
+            for (i in list.indices) {
+                val (rat, start) = list[i]
+                val end = list.getOrNull(i + 1)?.second ?: input.length
+                when (rat) {
+                    Rat.eutra ->
+                        eutra =
+                            input.substring(start + eutraIdentifier.toString().length - 1, end)
+
+                    Rat.eutra_nr ->
+                        eutraNr =
+                            input.substring(start + mrdcIdentifier.toString().length - 1, end)
+
+                    Rat.nr -> nr = input.substring(start + nrIdentifier.toString().length - 1, end)
+                    else -> {}
+                }
+            }
+            if (eutra.isNotBlank()) {
+                getAsn1Converter(Rat.eutra, converter!!)
+                    .convert(
+                        Rat.eutra.ratCapabilityIdentifier,
+                        eutra.byteInputStream(),
+                        formatWriter
+                    )
+                formatWriter.jsonNode?.let { ratContainerMap.put(Rat.eutra.toString(), it) }
+            }
+            if (eutraNr.isNotBlank() || nr.isNotBlank()) {
+                val nrConverter = getAsn1Converter(Rat.nr, converter!!)
+                if (eutraNr.isNotBlank()) {
+                    nrConverter.convert(
+                        Rat.eutra_nr.ratCapabilityIdentifier,
+                        eutraNr.byteInputStream(),
+                        formatWriter
+                    )
+                    formatWriter.jsonNode?.let { ratContainerMap.put(Rat.eutra_nr.toString(), it) }
+                }
+                if (nr.isNotBlank()) {
+                    nrConverter.convert(
+                        Rat.nr.ratCapabilityIdentifier,
+                        nr.byteInputStream(),
+                        formatWriter
+                    )
+                    formatWriter.jsonNode?.let { ratContainerMap.put(Rat.nr.toString(), it) }
+                }
+            }
+        }
+
+        val jsonOutput = JsonObject(ratContainerMap)
+
+        if (cmd.hasOption("uelog")) {
+            val outputFile = cmd.getOptionValue("uelog")
+            outputFile(jsonOutput.toString(), outputFile)
+        }
+
+        val jsonEutra = jsonOutput.getOrDefault(Rat.eutra.toString(), null) as? JsonObject
+        val jsonEutraNr = jsonOutput.getOrDefault(Rat.eutra_nr.toString(), null) as? JsonObject
+        val jsonNr = jsonOutput.getOrDefault(Rat.nr.toString(), null) as? JsonObject
+
+        return imports.parse(jsonEutra, jsonEutraNr, jsonNr)
     }
 }
