5a08c5f3e6d4fa9294f0fdae169fe1c5348e6b4f
handymenny <handymenny@outlook.com>
2023-03-12 18:08:24 +0100

    test: add tests for importmtklte

---
+++ src/test/java/it/smartphonecombo/uecapabilityparser/importer/lte/ImportMtkLteTest.kt
@@ -0,0 +1,38 @@
+package it.smartphonecombo.uecapabilityparser.importer.lte
+
+import it.smartphonecombo.uecapabilityparser.Utility
+import java.io.File
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class ImportMtkLteTest {
+
+    companion object {
+        val importMtkLte = ImportMTKLte()
+    }
+
+    private fun parse(inputFilename: String, oracleFilename: String) {
+        val path = "src/test/resources/mtkLte/"
+
+        val inputPath = "$path/input/$inputFilename"
+        val oraclePath = "$path/oracle/$oracleFilename"
+
+        val capabilities = importMtkLte.parse(inputPath)
+        val actualCsv = Utility.toCsv(capabilities).lines().dropLastWhile { it.isBlank() }
+        Utility.outputFile(actualCsv.joinToString("\n") + "\n", oraclePath)
+        val expectedCsv =
+            File(oraclePath).bufferedReader().readLines().dropLastWhile { it.isBlank() }
+
+        Assertions.assertLinesMatch(expectedCsv, actualCsv)
+    }
+
+    @Test
+    fun parsePreCaCombInfo() {
+        parse("PRE_CA_COMB_INFO.txt", "PRE_CA_COMB_INFO.csv")
+    }
+
+    @Test
+    fun parseUeCaCombInfo() {
+        parse("UE_CA_COMB_INFO.txt", "UE_CA_COMB_INFO.csv")
+    }
+}
