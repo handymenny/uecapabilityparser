f2b9f98915cc2aefb53406eba6a32210f155a5d1
handymenny <handymenny@outlook.com>
2023-03-21 15:10:26 +0100

    ImportCarrierPolicy: extract parseCombo, parseComponent and parseBcs from parse()

--- src/main/java/it/smartphonecombo/uecapabilityparser/importer/lte/ImportCarrierPolicy.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/importer/lte/ImportCarrierPolicy.kt
@@ -11,58 +11,84 @@ import java.io.InputStreamReader
 
 class ImportCarrierPolicy : ImportCapabilities {
 
-    private val regex = """(\d{1,3})([A-F])([24]{0,5})([A-F]?)""".toRegex()
-
     override fun parse(input: InputStream): Capabilities {
         val caBandCombosString = input.reader().use(InputStreamReader::readText)
+
         val listCombo =
             caBandCombosString
                 .split(';', '"')
                 .filter(String::isNotBlank)
-                .mapNotNull { x: String ->
-                    val bands = ArrayList<IComponent>()
-                    val components = x.split('-')
-                    val lastIndex = components.size - 1
-                    for (i in 0 until lastIndex) {
-                        val y = components[i].filterNot { it.isWhitespace() }
-                        val matchResult = regex.find(y)
-                        if (matchResult != null) {
-                            val groups = matchResult.groupValues
-                            val baseBand = groups[1].toInt()
-                            val bandwidthClass = groups[2][0]
-                            var result = groups[3]
-                            val mimo = if (result.isEmpty()) 0 else result.toInt()
-                            result = groups[4]
-                            val uplink = if (result.isEmpty()) '0' else result[0]
-                            bands.add(
-                                ComponentLte(baseBand, bandwidthClass, uplink, mimo, null, null)
-                            )
-                        }
-                    }
-
-                    if (bands.isEmpty()) {
-                        return@mapNotNull null
-                    }
-
-                    bands.sortWith(IComponent.defaultComparator.reversed())
-                    val combo = ComboLte(bands.toTypedArray())
-                    val bcsString = components[lastIndex].filterNot { it.isWhitespace() }
-                    if (bcsString.isNotEmpty() && bcsString != "mAll") {
-                        try {
-                            if (bcsString.startsWith('m')) {
-                                val bcs = bcsString.substring(1).toInt(16)
-                                combo.bcs = Utility.qcomBcsToArray(bcs)
-                            } else {
-                                val bcs = bcsString.toInt()
-                                combo.setSingleBcs(bcs)
-                            }
-                        } catch (ignored: NumberFormatException) {
-                            return@mapNotNull null
-                        }
-                    }
-                    combo
-                }
+                .mapNotNull(this::parseCombo)
                 .toList()
+
         return Capabilities(listCombo)
+    }
+
+    /** Converts the given comboString to a [ComboLte] Returns null if parsing fails */
+    private fun parseCombo(comboString: String): ComboLte? {
+        val components = comboString.split('-')
+        val parsedComponents = mutableListOf<IComponent>()
+
+        for (i in 0 until components.size - 1) {
+            val componentString = components[i].filterNot(Char::isWhitespace)
+            val component = parseComponent(componentString)
+            if (component != null) {
+                parsedComponents.add(component)
+            }
+        }
+
+        if (parsedComponents.isEmpty()) {
+            return null
+        }
+
+        val bcsString = components.last().filterNot(Char::isWhitespace)
+        val bcsArray =
+            try {
+                parseBcs(bcsString)
+            } catch (ignored: NumberFormatException) {
+                return null
+            }
+
+        val componentsArray =
+            parsedComponents.sortedWith(IComponent.defaultComparator.reversed()).toTypedArray()
+
+        return ComboLte(componentsArray, bcsArray)
+    }
+
+    // Regex used to extract the various parts of a component
+    private val componentRegex = """(\d{1,3})([A-F])([24]{0,5})([A-F]?)""".toRegex()
+
+    /** Converts the given componentString to a [ComponentLte] Returns null if parsing fails */
+    private fun parseComponent(componentString: String): ComponentLte? {
+        val result = componentRegex.find(componentString) ?: return null
+
+        val (_, baseBand, classDL, mimoDL, classUL) = result.groupValues
+
+        return ComponentLte(
+            baseBand.toInt(),
+            classDL.first(),
+            classUL.firstOrNull() ?: '0',
+            mimoDL.toIntOrNull() ?: 0,
+            null,
+            null
+        )
+    }
+
+    /**
+     * Converts the given bcsString to an array of BCSs.
+     * - If bcsString is empty or "mAll" returns an empty IntArray
+     * - If bcsString starts with m it parses it as Qualcomm MultiBcs
+     * - otherwise it returns an IntArray containing the integer extracted from bcsString
+     */
+    @Throws(NumberFormatException::class)
+    private fun parseBcs(bcsString: String): IntArray {
+        return if (bcsString == "mAll" || bcsString.isEmpty()) {
+            IntArray(0)
+        } else if (bcsString.startsWith('m')) {
+            val bcs = bcsString.substring(1).toInt(16)
+            Utility.qcomBcsToArray(bcs)
+        } else {
+            intArrayOf(bcsString.toInt())
+        }
     }
 }
