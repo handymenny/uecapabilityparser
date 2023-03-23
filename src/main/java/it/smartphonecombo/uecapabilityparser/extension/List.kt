42ce1450d00e156477008d8b9ef4ddd457872952
handymenny <handymenny@outlook.com>
2023-03-23 16:30:22 +0100

    ImportCapPrune: refactoring

---
+++ src/main/java/it/smartphonecombo/uecapabilityparser/extension/List.kt
@@ -0,0 +1,5 @@
+package it.smartphonecombo.uecapabilityparser.extension
+
+internal inline operator fun <T> List<T>.component6(): T = get(5)
+
+internal inline operator fun <T> List<T>.component7(): T = get(6)
--- src/main/java/it/smartphonecombo/uecapabilityparser/importer/nr/ImportCapPrune.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/importer/nr/ImportCapPrune.kt
@@ -5,74 +5,95 @@ import it.smartphonecombo.uecapabilityparser.bean.IComponent
 import it.smartphonecombo.uecapabilityparser.bean.lte.ComponentLte
 import it.smartphonecombo.uecapabilityparser.bean.nr.ComboNr
 import it.smartphonecombo.uecapabilityparser.bean.nr.ComponentNr
+import it.smartphonecombo.uecapabilityparser.extension.component6
+import it.smartphonecombo.uecapabilityparser.extension.component7
 import it.smartphonecombo.uecapabilityparser.importer.ImportCapabilities
 import java.io.InputStream
 import java.io.InputStreamReader
 
-class ImportCapPrune : ImportCapabilities {
-
-    private val regex =
-        """([bn])(\d{1,3})([A-Q]?)\[?([\d,]{0,8})]?([A-Q]?)\[?([\d,]{0,8})]?""".toRegex()
+object ImportCapPrune : ImportCapabilities {
 
     override fun parse(input: InputStream): Capabilities {
         val caBandCombosString = input.reader().use(InputStreamReader::readText)
+
         val listCombo =
-            caBandCombosString
-                .split(';')
-                .filter(String::isNotBlank)
-                .map { components: String ->
-                    val lteBands = ArrayList<IComponent>()
-                    val nrBands = ArrayList<IComponent>()
-                    components.split('-').forEach { y: String ->
-                        val matchResult = regex.find(y)
-                        if (matchResult != null) {
-                            val groups = matchResult.groupValues
-                            val baseBand = groups[2].toInt()
-                            var result = groups[3]
+            caBandCombosString.split(';').filter(String::isNotBlank).mapNotNull(::parseCombo)
 
-                            val bandwidthClass = if (result.isEmpty()) '0' else result[0]
-                            result = groups[5]
-                            val uplink = if (result.isEmpty()) '0' else result[0]
-
-                            val mimo = mimoParsing(groups[4])
-                            if (groups[1] == "b") {
-                                lteBands.add(
-                                    ComponentLte(baseBand, bandwidthClass, uplink, mimo, null, null)
-                                )
-                            } else {
-                                val ulMimo = mimoParsing(groups[6])
-                                nrBands.add(
-                                    ComponentNr(
-                                        baseBand,
-                                        bandwidthClass,
-                                        uplink,
-                                        mimo,
-                                        ulMimo,
-                                        null,
-                                        null
-                                    )
-                                )
-                            }
-                        }
-                    }
-                    lteBands.sortWith(IComponent.defaultComparator.reversed())
-                    nrBands.sortWith(IComponent.defaultComparator.reversed())
-                    val bandArray = lteBands.toTypedArray()
-                    val nrbandsArray = nrBands.toTypedArray()
-                    if (bandArray.isEmpty()) ComboNr(nrbandsArray)
-                    else ComboNr(bandArray, nrbandsArray)
-                }
-                .toList()
         val cap = Capabilities()
-        cap.enDcCombos = listCombo.filter { it.componentsLte.isNotEmpty() }
-        cap.nrCombos = listCombo.filter { it.componentsLte.isEmpty() }
+        val (enDcCombos, nrCombos) = listCombo.partition { it.componentsLte.isNotEmpty() }
+        cap.enDcCombos = enDcCombos
+        cap.nrCombos = nrCombos
         return cap
     }
 
-    private fun mimoParsing(mimo: String): Int {
-        if (mimo.isEmpty()) {
-            return 0
+    private fun parseCombo(comboString: String): ComboNr? {
+        val lteBands = mutableListOf<IComponent>()
+        val nrBands = mutableListOf<IComponent>()
+        val components = comboString.split('-')
+
+        for (componentString in components) {
+            when (val component = parseComponent(componentString)) {
+                is ComponentLte -> lteBands.add(component)
+                is ComponentNr -> nrBands.add(component)
+            }
         }
-        return mimo.split(",").first().toInt()
+
+        val lteBandsArray = lteBands.toTypedArray()
+        lteBandsArray.sortWith(IComponent.defaultComparator.reversed())
+        val nrBandsArray = nrBands.toTypedArray()
+        nrBandsArray.sortWith(IComponent.defaultComparator.reversed())
+
+        return if (lteBandsArray.isEmpty() && nrBandsArray.isEmpty()) {
+            null
+        } else if (lteBandsArray.isEmpty()) {
+            ComboNr(nrBandsArray)
+        } else {
+            ComboNr(lteBandsArray, nrBandsArray)
+        }
+    }
+
+    private val componentsRegex =
+        """([bn])(\d{1,3})([A-Q]?)\[?([\d,]{0,8})]?([A-Q]?)\[?([\d,]{0,8})]?""".toRegex()
+    private fun parseComponent(componentString: String): IComponent? {
+        val result = componentsRegex.find(componentString) ?: return null
+
+        val (_, type, baseBand, classDL, mimoDL, classUL, mimoUL) = result.groupValues
+
+        if (type == "b") {
+            return ComponentLte(
+                baseBand.toInt(),
+                bwClassParsing(classDL),
+                bwClassParsing(classUL),
+                mimoParsing(mimoDL),
+                null,
+                null
+            )
+        } else {
+            return ComponentNr(
+                baseBand.toInt(),
+                bwClassParsing(classDL),
+                bwClassParsing(classUL),
+                mimoParsing(mimoDL),
+                mimoParsing(mimoUL),
+                null,
+                null
+            )
+        }
+    }
+
+    private fun mimoParsing(mimo: String): Int {
+        return if (mimo.isEmpty()) {
+            0
+        } else {
+            mimo.split(",").first().toInt()
+        }
+    }
+
+    private fun bwClassParsing(bwClass: String): Char {
+        return if (bwClass.isEmpty()) {
+            '0'
+        } else {
+            bwClass[0]
+        }
     }
 }
