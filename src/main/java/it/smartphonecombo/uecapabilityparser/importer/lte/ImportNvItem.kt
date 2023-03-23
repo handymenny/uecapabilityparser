bea2765a579ea714caa6fd44b265f219c6907528
handymenny <handymenny@outlook.com>
2023-03-23 21:08:46 +0100

    ImportNvItem: add more comments

--- src/main/java/it/smartphonecombo/uecapabilityparser/importer/lte/ImportNvItem.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/importer/lte/ImportNvItem.kt
@@ -12,11 +12,33 @@ import it.smartphonecombo.uecapabilityparser.importer.ImportCapabilities
 import java.io.InputStream
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
+import kotlin.jvm.Throws
 
 private const val MAX_CC = 5
 
+/**
+ * A parser for Qualcomm NVItem 28874 (RFNV_LTE_CA_BW_CLASS_COMBO_I).
+ *
+ * This NVItem is found in the latest Qualcomm 4G modems (~ 2017 and beyond) and defines supported
+ * LTE Carrier Aggregations.
+ *
+ * This NVItem isn't found in Qualcomm 5G modems.
+ *
+ * Inspired by [28874Decoder](https://github.com/HandyMenny/28874Decoder)
+ */
 object ImportNvItem : ImportCapabilities {
 
+    /**
+     * This parser take as [input] an [InputStream] of a decompressed NVItem 28874.
+     *
+     * The output is a [Capabilities] with the list of parsed LTE combos stored in
+     * [lteCombos][Capabilities.lteCombos].
+     *
+     * It supports 28874 containing the following descriptor types: 137, 138, 201, 202, 333, 334.
+     *
+     * Throws an [IllegalArgumentException] if an invalid or unsupported descriptor type is found.
+     */
+    @Throws(IllegalArgumentException::class)
     override fun parse(input: InputStream): Capabilities {
         var dlComponents = emptyList<ComponentLte>()
         val listCombo = ArrayList<ComboLte>()
@@ -30,12 +52,15 @@ object ImportNvItem : ImportCapabilities {
                 333,
                 201,
                 137 -> {
+                    // Get DL Components
                     dlComponents = parseItem(byteBuffer, itemType)
                 }
                 334,
                 202,
                 138 -> {
+                    // Get UL Components
                     val ulComponents = parseItem(byteBuffer, itemType)
+                    // merge DL and UL Components
                     val bandArray = mergeAndSort(dlComponents, ulComponents)
                     listCombo.add(ComboLte(bandArray))
                 }
@@ -45,6 +70,13 @@ object ImportNvItem : ImportCapabilities {
         return Capabilities(listCombo)
     }
 
+    /**
+     * Parses a descriptor/item. It supports the following descriptor types: 137, 138, 201, 202,
+     * 333, 334.
+     *
+     * Throws an [IllegalArgumentException] if an invalid or unsupported descriptor type is found.
+     */
+    @Throws(IllegalArgumentException::class)
     private fun parseItem(input: ByteBuffer, descriptorType: Int): List<ComponentLte> {
         return when (descriptorType) {
             334 -> readComponents(input, hasMimo = true, hasMultiMimo = true, isDL = false)
@@ -57,6 +89,7 @@ object ImportNvItem : ImportCapabilities {
         }
     }
 
+    /** Read/Parse carrier components of a single descriptor from the given input. */
     private fun readComponents(
         input: ByteBuffer,
         hasMimo: Boolean = false,
@@ -100,6 +133,7 @@ object ImportNvItem : ImportCapabilities {
         return lteComponents
     }
 
+    /** Merge DL Components and UL Components */
     private fun mergeAndSort(
         dlComponents: List<ComponentLte>,
         ulComponents: List<ComponentLte>
@@ -118,6 +152,7 @@ object ImportNvItem : ImportCapabilities {
         return components
     }
 
+    /** Like map but returning a typedArray */
     private inline fun <reified T> List<T>.mapToTypedArray(transform: (T) -> T) =
         Array(size, init = { i -> transform(this[i]) })
 }
