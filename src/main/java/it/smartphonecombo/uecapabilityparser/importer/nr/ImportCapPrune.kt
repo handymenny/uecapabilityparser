1ca22e28e54f2750449cbf3665cf5c12b9c8eb47
handymenny <handymenny@outlook.com>
2023-03-24 12:22:22 +0100

    ImportCapPrune: add kdoc comments

--- src/main/java/it/smartphonecombo/uecapabilityparser/importer/nr/ImportCapPrune.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/importer/nr/ImportCapPrune.kt
@@ -11,8 +11,33 @@ import it.smartphonecombo.uecapabilityparser.importer.ImportCapabilities
 import java.io.InputStream
 import java.io.InputStreamReader
 
+/** A parser for NR Combinations as found in Qualcomm cap_prune */
 object ImportCapPrune : ImportCapabilities {
 
+    /**
+     * This parser take as [input] an [InputStream] containing NR Combinations using the same format
+     * used in Qualcomm cap_prune.
+     *
+     * In this format each combination is separated from the other by a ";".
+     *
+     * This combination consists of carrier components separated by "-".
+     *
+     * Each carrier component is composed of a character that is b for LTE combos and n for Combos,
+     * followed by a number representing the band, followed by a character representing the downlink
+     * bandwidth class, followed by an array representing the number of RX antennas (ex. [4,2]),
+     * followed by a character representing the uplink bandwidth class, followed by an array
+     * representing the number of TX antennas (ex. [2,2]),
+     *
+     * The number of RX antennas, the uplink bandwidth class and the number of TX antennas are
+     * optional.
+     *
+     * The output is a [Capabilities] with the list of parsed NR CA combos stored in
+     * [nrCombos][Capabilities.nrCombos] and the list of parsed EN DC combos stored in
+     * [enDcCombos][Capabilities.enDcCombos].
+     *
+     * This parser doesn't support NR DC or SUL combos, if you have an example of cap_prune
+     * containing such combos, please share it with info at smartphonecombo dot it
+     */
     override fun parse(input: InputStream): Capabilities {
         val caBandCombosString = input.reader().use(InputStreamReader::readText)
 
@@ -26,6 +51,7 @@ object ImportCapPrune : ImportCapabilities {
         return cap
     }
 
+    /** Converts the given comboString to a [ComboNr] Returns null if parsing fails */
     private fun parseCombo(comboString: String): ComboNr? {
         val lteBands = mutableListOf<IComponent>()
         val nrBands = mutableListOf<IComponent>()
@@ -52,8 +78,10 @@ object ImportCapPrune : ImportCapabilities {
         }
     }
 
+    // Regex used to extract the various parts of a component
     private val componentsRegex =
         """([bn])(\d{1,3})([A-Q]?)\[?([\d,]{0,8})]?([A-Q]?)\[?([\d,]{0,8})]?""".toRegex()
+    /** Converts the given componentString to a [IComponent]. Returns null if parsing fails */
     private fun parseComponent(componentString: String): IComponent? {
         val result = componentsRegex.find(componentString) ?: return null
 
@@ -81,6 +109,7 @@ object ImportCapPrune : ImportCapabilities {
         }
     }
 
+    /** Extract mimo from the given string. Only the first mimo found is returned. */
     private fun mimoParsing(mimo: String): Int {
         return if (mimo.isEmpty()) {
             0
@@ -89,6 +118,7 @@ object ImportCapPrune : ImportCapabilities {
         }
     }
 
+    /** Returns the first character of [bwClass] or '0' if empty. */
     private fun bwClassParsing(bwClass: String): Char {
         return if (bwClass.isEmpty()) {
             '0'
