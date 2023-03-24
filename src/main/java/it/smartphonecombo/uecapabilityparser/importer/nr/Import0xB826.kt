23f0d4dbb9833aedbf1b40f2a3811d2e64003d14
handymenny <handymenny@outlook.com>
2023-03-24 17:43:00 +0100

    Import0xB826: move parseCombo logic in a separate function

--- src/main/java/it/smartphonecombo/uecapabilityparser/importer/nr/Import0xB826.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/importer/nr/Import0xB826.kt
@@ -18,8 +18,6 @@ class Import0xB826 : ImportCapabilities {
     override fun parse(input: InputStream): Capabilities {
         val combos = Capabilities()
         val listCombo = ArrayList<ComboNr>()
-        var endc = false
-        var nrdc = false
         val byteArray = input.use(InputStream::readBytes)
         val byteBuffer = ByteBuffer.wrap(byteArray)
         byteBuffer.order(ByteOrder.LITTLE_ENDIAN)
@@ -78,149 +76,162 @@ class Import0xB826 : ImportCapabilities {
 
         var comboN = 0
         while (comboN < numCombos && byteBuffer.remaining() > 0) {
-            if (version >= 8) {
-                byteBuffer.skipBytes(3)
-            }
-            var numBands = byteBuffer.readUnsignedByte()
-            numBands =
-                when (version) {
-                    1,
-                    2 -> numBands
-                    in 3..7 -> numBands ushr 1
-                    else -> (numBands ushr 3) and 0x0F
-                }
-            val bands = mutableListOf<IComponent>()
-            var nrbands = mutableListOf<IComponent>()
-            var nrdcbands = mutableListOf<IComponent>()
-            when (version) {
-                6,
-                8 -> byteBuffer.skipBytes(1)
-                7 -> byteBuffer.skipBytes(3)
-                in 9..13 -> byteBuffer.skipBytes(9)
-                14 -> byteBuffer.skipBytes(25)
-            }
-            for (i in 0 until numBands) {
-                var band: Int
-                val mixed = byteBuffer.readUnsignedShort()
-                var temp: Int
-                if (version >= 8) {
-                    temp = mixed ushr 9 and 0x1F
-                    band = mixed and 0x1FF
-                } else {
-                    band = mixed
-                    temp = byteBuffer.readUnsignedByte()
-                }
-                val bwclass = (temp ushr 1).toBwClass()
-                val isNr = temp % 2 == 1
-
-                val component =
-                    if (isNr) {
-                        ComponentNr(band)
-                    } else {
-                        ComponentLte(band)
-                    }
-                component.classDL = bwclass
-                if (version >= 8) {
-                    temp = byteBuffer.readUnsignedByte()
-                    var mimo = temp shl 1
-                    mimo = mimo and 0x7F
-                    mimo += mixed ushr 15
-                    component.mimoDL = getMimoFromIndex(mimo)
-                    temp = temp ushr 6
-                    val temp2 = byteBuffer.readUnsignedByte()
-                    temp += temp2 shl 2
-                    temp = temp and 0x1F
-                    val mimoUL = (temp2 ushr 3) and 0x7F
-                    component.classUL = temp.toBwClass()
-                    component.mimoUL = getMimoFromIndex(mimoUL)
-                    temp = byteBuffer.readUnsignedByte()
-                    val modUL = (temp shr 1) and 0x3
-                    component.modUL = getQamFromIndex(modUL)
-                } else {
-                    component.mimoDL = getMimoFromIndex(byteBuffer.readUnsignedByte())
-                    temp = byteBuffer.readUnsignedByte() ushr 1
-                    component.classUL = temp.toBwClass()
-                    val mimoUL = byteBuffer.readUnsignedByte()
-                    component.mimoUL = getMimoFromIndex(mimoUL)
-                    temp = byteBuffer.readUnsignedByte()
-                    val modUL = temp
-                    component.modUL = getQamFromIndex(modUL)
-                }
-
-                if (isNr) {
-                    val nrband = component as ComponentNr
-                    if (version >= 8) {
-                        var scsIndex = temp
-                        temp = byteBuffer.readUnsignedByte()
-                        scsIndex = scsIndex ushr 7
-                        scsIndex += temp and 3 shl 1
-                        nrband.scs = 15 * (1 shl (scsIndex and 0x000F) - 1)
-                        val maxBWindex = temp shr 2 and 0x1F
-                        nrband.maxBandwidth = getBWFromIndexV8(maxBWindex)
-                        byteBuffer.skipBytes(2)
-                    } else if (version >= 6) {
-                        byteBuffer.skipBytes(1)
-                        temp = byteBuffer.readUnsignedShort()
-                        val scsIndex = temp
-                        nrband.scs = 15 * (1 shl (scsIndex and 0x000F) - 1)
-                        nrband.maxBandwidth = getBWFromIndex(temp ushr 6 and 0x1F)
-                    } else if (version > 2) {
-                        byteBuffer.skipBytes(1)
-                        nrband.scs = 15 * (1 shl byteBuffer.readUnsignedByte() - 1)
-                        nrband.maxBandwidth = byteBuffer.readUnsignedByte() shl 2
-                    } else {
-                        byteBuffer.skipBytes(1)
-                        nrband.scs = 15 * (1 + byteBuffer.readUnsignedByte())
-                        nrband.maxBandwidth = byteBuffer.readUnsignedByte() shl 2
-                    }
-                    nrbands.add(component)
-                } else {
-                    endc = true
-                    byteBuffer.skipBytes(3)
-                    bands.add(component)
-                }
-            }
-            /*
-             * We assume that 0xb826 without explicit combo type in source
-             * don't support NR CA FR1-FR2.
-             */
-            if (!endc && !source.equals("RF_NRCA")) {
-                val (fr2bands, fr1bands) = nrbands.partition { (it as ComponentNr).isFR2 }
-
-                if (fr2bands.isNotEmpty() && fr1bands.isNotEmpty()) {
-                    nrdc = true
-                    nrbands = fr1bands.toMutableList()
-                    nrdcbands = fr2bands.toMutableList()
-                }
-            }
-
-            val bandArray = bands.sortedWith(IComponent.defaultComparator.reversed()).toTypedArray()
-            val nrbandsArray =
-                nrbands.sortedWith(IComponent.defaultComparator.reversed()).toTypedArray()
-            val nrdcbandsArray =
-                nrdcbands.sortedWith(IComponent.defaultComparator.reversed()).toTypedArray()
-            val newCombo =
-                if (endc) {
-                    ComboNr(bandArray, nrbandsArray)
-                } else if (nrdc) {
-                    ComboNr(nrbandsArray, nrdcbandsArray)
-                } else {
-                    ComboNr(nrbandsArray)
-                }
-            listCombo.add(newCombo)
+            val combo = parseCombo(byteBuffer, version, source)
+            listCombo.add(combo)
             comboN++
         }
+
         if (debug) {
             println(listCombo)
         }
-        if (endc) {
-            combos.enDcCombos = listCombo
-        } else if (nrdc) {
-            combos.nrDcCombos = listCombo
-        } else {
-            combos.nrCombos = listCombo
+
+        if (listCombo.isNotEmpty()) {
+            if (listCombo.first().isEnDc) {
+                combos.enDcCombos = listCombo
+            } else if (listCombo.first().isNrDc) {
+                combos.nrDcCombos = listCombo
+            } else {
+                combos.nrCombos = listCombo
+            }
         }
         return combos
+    }
+
+    private fun parseCombo(
+        byteBuffer: ByteBuffer,
+        version: Int,
+        source: String?,
+    ): ComboNr {
+        var endc = false
+        var nrdc = false
+        if (version >= 8) {
+            byteBuffer.skipBytes(3)
+        }
+        var numBands = byteBuffer.readUnsignedByte()
+        numBands =
+            when (version) {
+                1,
+                2 -> numBands
+                in 3..7 -> numBands ushr 1
+                else -> (numBands ushr 3) and 0x0F
+            }
+        val bands = mutableListOf<IComponent>()
+        var nrbands = mutableListOf<IComponent>()
+        var nrdcbands = mutableListOf<IComponent>()
+        when (version) {
+            6,
+            8 -> byteBuffer.skipBytes(1)
+            7 -> byteBuffer.skipBytes(3)
+            in 9..13 -> byteBuffer.skipBytes(9)
+            14 -> byteBuffer.skipBytes(25)
+        }
+        for (i in 0 until numBands) {
+            var band: Int
+            val mixed = byteBuffer.readUnsignedShort()
+            var temp: Int
+            if (version >= 8) {
+                temp = mixed ushr 9 and 0x1F
+                band = mixed and 0x1FF
+            } else {
+                band = mixed
+                temp = byteBuffer.readUnsignedByte()
+            }
+            val bwclass = (temp ushr 1).toBwClass()
+            val isNr = temp % 2 == 1
+
+            val component =
+                if (isNr) {
+                    ComponentNr(band)
+                } else {
+                    ComponentLte(band)
+                }
+            component.classDL = bwclass
+            if (version >= 8) {
+                temp = byteBuffer.readUnsignedByte()
+                var mimo = temp shl 1
+                mimo = mimo and 0x7F
+                mimo += mixed ushr 15
+                component.mimoDL = getMimoFromIndex(mimo)
+                temp = temp ushr 6
+                val temp2 = byteBuffer.readUnsignedByte()
+                temp += temp2 shl 2
+                temp = temp and 0x1F
+                val mimoUL = (temp2 ushr 3) and 0x7F
+                component.classUL = temp.toBwClass()
+                component.mimoUL = getMimoFromIndex(mimoUL)
+                temp = byteBuffer.readUnsignedByte()
+                val modUL = (temp shr 1) and 0x3
+                component.modUL = getQamFromIndex(modUL)
+            } else {
+                component.mimoDL = getMimoFromIndex(byteBuffer.readUnsignedByte())
+                temp = byteBuffer.readUnsignedByte() ushr 1
+                component.classUL = temp.toBwClass()
+                val mimoUL = byteBuffer.readUnsignedByte()
+                component.mimoUL = getMimoFromIndex(mimoUL)
+                temp = byteBuffer.readUnsignedByte()
+                val modUL = temp
+                component.modUL = getQamFromIndex(modUL)
+            }
+
+            if (isNr) {
+                val nrband = component as ComponentNr
+                if (version >= 8) {
+                    var scsIndex = temp
+                    temp = byteBuffer.readUnsignedByte()
+                    scsIndex = scsIndex ushr 7
+                    scsIndex += temp and 3 shl 1
+                    nrband.scs = 15 * (1 shl (scsIndex and 0x000F) - 1)
+                    val maxBWindex = temp shr 2 and 0x1F
+                    nrband.maxBandwidth = getBWFromIndexV8(maxBWindex)
+                    byteBuffer.skipBytes(2)
+                } else if (version >= 6) {
+                    byteBuffer.skipBytes(1)
+                    temp = byteBuffer.readUnsignedShort()
+                    val scsIndex = temp
+                    nrband.scs = 15 * (1 shl (scsIndex and 0x000F) - 1)
+                    nrband.maxBandwidth = getBWFromIndex(temp ushr 6 and 0x1F)
+                } else if (version > 2) {
+                    byteBuffer.skipBytes(1)
+                    nrband.scs = 15 * (1 shl byteBuffer.readUnsignedByte() - 1)
+                    nrband.maxBandwidth = byteBuffer.readUnsignedByte() shl 2
+                } else {
+                    byteBuffer.skipBytes(1)
+                    nrband.scs = 15 * (1 + byteBuffer.readUnsignedByte())
+                    nrband.maxBandwidth = byteBuffer.readUnsignedByte() shl 2
+                }
+                nrbands.add(component)
+            } else {
+                endc = true
+                byteBuffer.skipBytes(3)
+                bands.add(component)
+            }
+        }
+        /*
+         * We assume that 0xb826 without explicit combo type in source
+         * don't support NR CA FR1-FR2.
+         */
+        if (!endc && !source.equals("RF_NRCA")) {
+            val (fr2bands, fr1bands) = nrbands.partition { (it as ComponentNr).isFR2 }
+
+            if (fr2bands.isNotEmpty() && fr1bands.isNotEmpty()) {
+                nrdc = true
+                nrbands = fr1bands.toMutableList()
+                nrdcbands = fr2bands.toMutableList()
+            }
+        }
+
+        val bandArray = bands.sortedWith(IComponent.defaultComparator.reversed()).toTypedArray()
+        val nrbandsArray =
+            nrbands.sortedWith(IComponent.defaultComparator.reversed()).toTypedArray()
+        val nrdcbandsArray =
+            nrdcbands.sortedWith(IComponent.defaultComparator.reversed()).toTypedArray()
+        return if (endc) {
+            ComboNr(bandArray, nrbandsArray)
+        } else if (nrdc) {
+            ComboNr(nrbandsArray, nrdcbandsArray)
+        } else {
+            ComboNr(nrbandsArray)
+        }
     }
 
     private fun getMimoFromIndex(index: Int): Int {
