50b51553c1df78d0d5682d1eafc5e0544ee24904
handymenny <handymenny@outlook.com>
2023-03-25 10:28:36 +0100

    extension: ByteBuffer: add BufferUnderflowException

--- src/main/java/it/smartphonecombo/uecapabilityparser/extension/ByteBuffer.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/extension/ByteBuffer.kt
@@ -1,15 +1,24 @@
 package it.smartphonecombo.uecapabilityparser.extension
 
+import java.nio.BufferUnderflowException
 import java.nio.ByteBuffer
+import kotlin.jvm.Throws
 
+@Throws(BufferUnderflowException::class)
 internal fun ByteBuffer.skipBytes(n: Int): ByteBuffer {
-    return this.position(this.position() + n)
+    try {
+        return this.position(this.position() + n)
+    } catch (ignored: IllegalArgumentException) {
+        throw BufferUnderflowException()
+    }
 }
 
+@Throws(BufferUnderflowException::class)
 internal fun ByteBuffer.readUnsignedByte(): Int {
     return this.get().toUByte().toInt()
 }
 
+@Throws(BufferUnderflowException::class)
 internal fun ByteBuffer.readUnsignedShort(): Int {
     return this.short.toUShort().toInt()
 }
