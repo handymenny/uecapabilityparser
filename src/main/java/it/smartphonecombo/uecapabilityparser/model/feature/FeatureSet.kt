592871319858b0082b59b28ea7482c2d86fe28d4
handymenny <handymenny@outlook.com>
2023-03-30 17:47:58 +0200

    FeautureSet: Use default toString method

--- src/main/java/it/smartphonecombo/uecapabilityparser/model/feature/FeatureSet.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/model/feature/FeatureSet.kt
@@ -2,8 +2,4 @@ package it.smartphonecombo.uecapabilityparser.model.feature
 
 import it.smartphonecombo.uecapabilityparser.model.LinkDirection
 
-data class FeatureSet(val featureSetsPerCC: List<FeaturePerCCLte>?, val type: LinkDirection) {
-    override fun toString(): String {
-        return "FeatureSet(featureSetsPerCC=$featureSetsPerCC, type=$type)"
-    }
-}
+data class FeatureSet(val featureSetsPerCC: List<FeaturePerCCLte>?, val type: LinkDirection)
