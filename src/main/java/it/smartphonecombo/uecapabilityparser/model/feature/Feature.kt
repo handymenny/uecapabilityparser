5cf7d9c23a2d7f21fa3ff9646b9a60864809dd95
handymenny <handymenny@outlook.com>
2023-03-30 17:47:15 +0200

    Feature: use default toString method

--- src/main/java/it/smartphonecombo/uecapabilityparser/model/feature/Feature.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/model/feature/Feature.kt
@@ -1,7 +1,3 @@
 package it.smartphonecombo.uecapabilityparser.model.feature
 
-data class Feature(val isNR: Boolean, val downlink: Int, val uplink: Int) {
-    override fun toString(): String {
-        return "DL: " + downlink + " UL: " + uplink + if (isNR) " NR" else " EUTRA"
-    }
-}
+data class Feature(val isNR: Boolean, val downlink: Int, val uplink: Int)
