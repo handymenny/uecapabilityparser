3433df25cecc8c658e5239df8cc24b92b4c616fc
handymenny <handymenny@outlook.com>
2023-05-06 17:38:36 +0200

    Capabilities: set required fields

--- src/main/java/it/smartphonecombo/uecapabilityparser/model/Capabilities.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/model/Capabilities.kt
@@ -7,13 +7,11 @@ import it.smartphonecombo.uecapabilityparser.model.combo.ComboNr
 import it.smartphonecombo.uecapabilityparser.model.combo.ComboNrDc
 import it.smartphonecombo.uecapabilityparser.model.component.ComponentLte
 import it.smartphonecombo.uecapabilityparser.util.Property
-import kotlinx.serialization.EncodeDefault
-import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Required
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-@OptIn(ExperimentalSerializationApi::class)
 data class Capabilities(
     @SerialName("lteca") var lteCombos: List<ComboLte> = emptyList(),
     @SerialName("lteBands") var lteBands: List<ComponentLte> = emptyList(),
@@ -25,12 +23,12 @@ data class Capabilities(
     @SerialName("endc") var enDcCombos: List<ComboEnDc> = emptyList(),
     @SerialName("nrca") var nrCombos: List<ComboNr> = emptyList(),
     @SerialName("nrdc") var nrDcCombos: List<ComboNrDc> = emptyList(),
-    @SerialName("logType") var logType: String = "",
-    @EncodeDefault(EncodeDefault.Mode.ALWAYS)
+    @Required @SerialName("logType") var logType: String = "",
+    @Required
     @SerialName("parserVersion")
     var parserVersion: String = Property.getProperty("project.version") ?: "",
-    @SerialName("timestamp") var timestamp: Long = 0,
-    @SerialName("metadata") val metadata: MutableMap<String, String> = mutableMapOf()
+    @Required @SerialName("timestamp") var timestamp: Long = 0,
+    @Required @SerialName("metadata") val metadata: MutableMap<String, String> = mutableMapOf()
 ) {
     fun setMetadata(key: String, value: Any) {
         metadata[key] = value.toString()
