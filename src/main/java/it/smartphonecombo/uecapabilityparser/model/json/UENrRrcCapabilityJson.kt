8c8ddc3a4afda1a6532bc935c85ca28441dac729
handymenny <handymenny@outlook.com>
2023-08-05 15:17:31 +0200

    capability json: add capabilityV1560

--- src/main/java/it/smartphonecombo/uecapabilityparser/model/json/UENrRrcCapabilityJson.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/model/json/UENrRrcCapabilityJson.kt
@@ -1,12 +1,21 @@
 package it.smartphonecombo.uecapabilityparser.model.json
 
+import it.smartphonecombo.uecapabilityparser.extension.getObjectAtPath
+import it.smartphonecombo.uecapabilityparser.extension.repeat
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 
 sealed interface UENrRrcCapabilityJson {
     val rootJson: JsonElement
+    val nrRrcCapabilityV1560: JsonObject?
 }
 
-class UEMrdcCapabilityJson(override val rootJson: JsonObject) : UENrRrcCapabilityJson
+class UEMrdcCapabilityJson(override val rootJson: JsonObject) : UENrRrcCapabilityJson {
+    override val nrRrcCapabilityV1560 =
+        rootJson.getObjectAtPath("nonCriticalExtension".repeat(4, "."))
+}
 
-class UENrCapabilityJson(override val rootJson: JsonObject) : UENrRrcCapabilityJson
+class UENrCapabilityJson(override val rootJson: JsonObject) : UENrRrcCapabilityJson {
+    override val nrRrcCapabilityV1560 =
+        rootJson.getObjectAtPath("nonCriticalExtension".repeat(4, "."))
+}
