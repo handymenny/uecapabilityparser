8da8a0cd52b12873147220040fbaca2c944ad61b
handymenny <handymenny@outlook.com>
2023-08-30 11:53:35 +0200

    Import0xB0CDTest: switch to json oracles

--- src/test/java/it/smartphonecombo/uecapabilityparser/importer/Import0xB0CDTest.kt
+++ src/test/java/it/smartphonecombo/uecapabilityparser/importer/Import0xB0CDTest.kt
@@ -1,63 +1,65 @@
 package it.smartphonecombo.uecapabilityparser.importer
 
-import it.smartphonecombo.uecapabilityparser.util.Output
+import it.smartphonecombo.uecapabilityparser.model.Capabilities
 import java.io.File
+import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 internal class Import0xB0CDTest {
+    private val path = "src/test/resources/0xB0CD/"
 
     private fun parse(inputFilename: String, oracleFilename: String) {
-        val path = "src/test/resources/0xB0CD/"
+        val filePath = "$path/input/$inputFilename"
+        val actual = Import0xB0CD.parse(File(filePath).readBytes())
+        val expected =
+            Json.decodeFromString<Capabilities>(File("$path/oracle/$oracleFilename").readText())
 
-        val inputPath = "$path/input/$inputFilename"
-        val oraclePath = "$path/oracle/$oracleFilename"
+        // Override dynamic properties
+        expected.parserVersion = actual.parserVersion
+        expected.timestamp = actual.timestamp
+        expected.id = actual.id
 
-        val capabilities = Import0xB0CD.parse(File(inputPath).readBytes())
-        val actualCsv = Output.toCsv(capabilities).lines().dropLastWhile { it.isBlank() }
-        val expectedCsv =
-            File(oraclePath).bufferedReader().readLines().dropLastWhile { it.isBlank() }
-
-        Assertions.assertLinesMatch(expectedCsv, actualCsv)
+        Assertions.assertEquals(expected, actual)
     }
 
     @Test
     fun parse0xB0CDv32() {
-        parse("v32.txt", "v32.csv")
+        parse("v32.txt", "v32.json")
     }
 
     @Test
     fun parse0xB0CDv40() {
-        parse("v40.txt", "v40.csv")
+        parse("v40.txt", "v40.json")
     }
 
     @Test
     fun parse0xB0CDv41() {
-        parse("v41.txt", "v41.csv")
+        parse("v41.txt", "v41.json")
     }
 
     @Test
     fun parse0xB0CDv32Multi() {
-        parse("v32multi.txt", "v32multi.csv")
+        parse("v32multi.txt", "v32multi.json")
     }
 
     @Test
     fun parse0xB0CDv40Multi() {
-        parse("v40multi.txt", "v40multi.csv")
+        parse("v40multi.txt", "v40multi.json")
     }
 
     @Test
     fun parse0xB0CDv41Multi() {
-        parse("v41multi.txt", "v41multi.csv")
+        parse("v41multi.txt", "v41multi.json")
     }
 
     @Test
     fun parse0xB0CDv40MixedMimo() {
-        parse("v40mixedMimo.txt", "v40mixedMimo.csv")
+        parse("v40mixedMimo.txt", "v40mixedMimo.json")
     }
 
     @Test
     fun parse0xB0CDv41MixedMimo() {
-        parse("v41mixedMimo.txt", "v41mixedMimo.csv")
+        parse("v41mixedMimo.txt", "v41mixedMimo.json")
     }
 }
