44621b634d6c2df56b71c9193b8f51cfaf655db8
handymenny <handymenny@outlook.com>
2023-09-17 14:02:25 +0200

    add reparse library tests

--- src/test/java/it/smartphonecombo/uecapabilityparser/UtilityForTests.kt
+++ src/test/java/it/smartphonecombo/uecapabilityparser/UtilityForTests.kt
@@ -6,7 +6,14 @@ import it.smartphonecombo.uecapabilityparser.model.PowerClass
 import it.smartphonecombo.uecapabilityparser.model.band.BandBoxed
 import it.smartphonecombo.uecapabilityparser.model.filter.BandFilterLte
 import it.smartphonecombo.uecapabilityparser.model.filter.BandFilterNr
+import java.io.File
 import java.io.InputStream
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.Comparator
+import java.util.stream.Collectors
+import kotlin.io.path.Path
+import kotlin.math.abs
 
 object UtilityForTests {
 
@@ -41,5 +48,46 @@ object UtilityForTests {
 
     internal fun String.toBwClass(): BwClass {
         return BwClass.valueOf(this)
+    }
+
+    internal fun Path.listFilesRecursively(): List<File>? {
+        return Files.walk(this)
+            .sorted(Comparator.reverseOrder())
+            .map(Path::toFile)
+            .collect(Collectors.toList())
+    }
+
+    /** Check if the given directories have the same tree and if files have similar sizes */
+    internal fun dirsSimilar(expected: String, actual: String): Boolean {
+        val pathA = Path(expected)
+        val pathB = Path(actual)
+        val dirA = pathA.toFile()
+        val dirB = pathB.toFile()
+        val filesA = pathA.listFilesRecursively()
+        val filesB = pathB.listFilesRecursively()
+
+        if (filesA == null && filesB == null) {
+            return true
+        } else if (filesA == null || filesB == null) {
+            return false
+        } else if (filesA.size != filesB.size) {
+            return false
+        }
+
+        val zip = filesA.zip(filesB)
+
+        val result =
+            zip.all { (fileA, fileB) ->
+                if (fileA.relativeTo(dirA) != fileB.relativeTo(dirB)) {
+                    false
+                } else if (fileA.isDirectory && fileB.isDirectory) {
+                    true
+                } else {
+                    val aSize = fileA.readBytes().size
+                    val bSize = fileB.readBytes().size
+                    abs(aSize - bSize) < aSize / 100
+                }
+            }
+        return result
     }
 }
---
+++ src/test/java/it/smartphonecombo/uecapabilityparser/server/ServerModeReparseTest.kt
@@ -0,0 +1,105 @@
+package it.smartphonecombo.uecapabilityparser.server
+
+import it.smartphonecombo.uecapabilityparser.UtilityForTests.dirsSimilar
+import it.smartphonecombo.uecapabilityparser.cli.Main
+import it.smartphonecombo.uecapabilityparser.util.Config
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.*
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class ServerModeReparseTest {
+    private val resourcesPath = "src/test/resources/server"
+    private val tmpStorePath = UUID.randomUUID().toString() + "-tmp"
+
+    @BeforeEach
+    fun setup() {
+        try {
+            deleteDirectory(tmpStorePath)
+        } catch (_: Exception) {}
+        try {
+            copyDirectory("$resourcesPath/inputForReparse", tmpStorePath)
+        } catch (_: Exception) {}
+    }
+
+    @AfterEach
+    fun teardown() {
+        try {
+            deleteDirectory(tmpStorePath)
+        } catch (_: Exception) {}
+        Config.clear()
+    }
+
+    @Test
+    fun testReparseAuto() {
+        test(
+            arrayOf("server", "-p", "0", "--store", tmpStorePath, "--reparse", "auto"),
+            "$resourcesPath/oracleForReparse/auto"
+        )
+    }
+
+    @Test
+    fun testReparseAutoCompression() {
+        test(
+            arrayOf(
+                "server",
+                "-p",
+                "0",
+                "--store",
+                tmpStorePath,
+                "--reparse",
+                "auto",
+                "--compression"
+            ),
+            "$resourcesPath/oracleForReparse/autoCompress"
+        )
+    }
+
+    @Test
+    fun testReparseForce() {
+        test(
+            arrayOf("server", "-p", "0", "--store", tmpStorePath, "--reparse", "force"),
+            "$resourcesPath/oracleForReparse/force"
+        )
+    }
+
+    @Test
+    fun testReparseForceCompression() {
+        test(
+            arrayOf(
+                "server",
+                "-p",
+                "0",
+                "--store",
+                tmpStorePath,
+                "--reparse",
+                "force",
+                "--compression"
+            ),
+            "$resourcesPath/oracleForReparse/forceCompress"
+        )
+    }
+
+    fun test(args: Array<String>, oraclePath: String) {
+        Main.main(args)
+        assertTrue(dirsSimilar(oraclePath, tmpStorePath))
+    }
+
+    private fun deleteDirectory(path: String) {
+        return Files.walk(Path.of(path))
+            .sorted(Comparator.reverseOrder())
+            .map(Path::toFile)
+            .forEach(File::delete)
+    }
+
+    private fun copyDirectory(src: String, dst: String) {
+        val srcFile = File(src)
+        val dstFile = File(dst)
+
+        srcFile.copyRecursively(dstFile)
+    }
+}
