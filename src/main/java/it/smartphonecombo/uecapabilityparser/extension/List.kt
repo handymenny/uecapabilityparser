handymenny/uecapabilityparser

commit 502a8e7114a38b4a6424b46703bc4d1ed259e681

csv: add NR BW and SCS for uplink

src/main/java/it/smartphonecombo/uecapabilityparser/extension/List.kt
@@ -48,12 +48,18 @@ internal fun List<ComponentNr>.populateCsvStringBuilders(
 
         if (component.classUL != BwClass.NONE) {
             ulCount++
-            nrUlBwMod
-                .append(component.band)
-                .append(component.classUL)
-                .append(separator)
-                .append(component.modUL)
-                .append(separator)
+            nrUlBwMod.append(component.band).append(component.classUL).append(separator)
+
+            if (component.maxBandwidthUl != EmptyBandwidth) {
+                nrUlBwMod.append(component.maxBandwidthUl)
+            }
+            nrUlBwMod.append(separator)
+
+            if (component.scs != 0) {
+                nrUlBwMod.append(component.scs)
+            }
+
+            nrUlBwMod.append(separator).append(component.modUL).append(separator)
 
             if (component.mimoUL != EmptyMimo) {
                 nrMimoUl.append(component.mimoUL)

src/main/java/it/smartphonecombo/uecapabilityparser/model/combo/ComboEnDc.kt
@@ -86,14 +86,14 @@ data class ComboEnDc(
         val compact = this.toCompactStr() + separator
 
         val nrBandBwScs = StringBuilder()
-        val nrUlBwMod = StringBuilder()
+        val nrUlBwScsMod = StringBuilder()
         val nrMimoDl = StringBuilder()
         val nrMimoUl = StringBuilder()
 
         componentsNr.populateCsvStringBuilders(
             nrBandBwScs,
             nrMimoDl,
-            nrUlBwMod,
+            nrUlBwScsMod,
             nrMimoUl,
             nrDlCC,
             nrUlCC,
@@ -132,6 +132,6 @@ data class ComboEnDc(
 
         repeat(lteUlCC - ulLteCount) { IO.appendSeparator(separator, lteUl, lteUl, lteMimoUl) }
 
-        return "$compact$lteDl$lteUl$nrBandBwScs$nrUlBwMod$lteMimoDl$nrMimoDl$lteMimoUl$nrMimoUl$bcs;$bcsEutra;$bcsIntraEnDc"
+        return "$compact$lteDl$lteUl$nrBandBwScs$nrUlBwScsMod$lteMimoDl$nrMimoDl$lteMimoUl$nrMimoUl$bcs;$bcsEutra;$bcsIntraEnDc"
     }
 }

src/main/java/it/smartphonecombo/uecapabilityparser/model/combo/ComboNr.kt
@@ -55,21 +55,21 @@ data class ComboNr(
         val compact = this.toCompactStr() + separator
 
         val nrBandBwScs = StringBuilder()
-        val nrUlBwMod = StringBuilder()
+        val nrUlBwScsMod = StringBuilder()
         val nrMimoDl = StringBuilder()
         val nrMimoUl = StringBuilder()
 
         componentsNr.populateCsvStringBuilders(
             nrBandBwScs,
             nrMimoDl,
-            nrUlBwMod,
+            nrUlBwScsMod,
             nrMimoUl,
             nrDlCC,
             nrUlCC,
             separator
         )
 
-        return "$compact$nrBandBwScs$nrUlBwMod$nrMimoDl$nrMimoUl$bcs"
+        return "$compact$nrBandBwScs$nrUlBwScsMod$nrMimoDl$nrMimoUl$bcs"
     }
 
     fun copy(featureSet: Int): ComboNr {

src/main/java/it/smartphonecombo/uecapabilityparser/model/combo/ComboNrDc.kt
@@ -63,18 +63,18 @@ data class ComboNrDc(
         val compact = this.toCompactStr() + separator
 
         val nrBandBwScs = StringBuilder()
-        val nrUlBwMod = StringBuilder()
+        val nrUlBwScsMod = StringBuilder()
         val nrMimoDl = StringBuilder()
         val nrMimoUl = StringBuilder()
         val nrDcBandBwScs = StringBuilder()
-        val nrDcUlBwMod = StringBuilder()
+        val nrDcUlBwScsMod = StringBuilder()
         val nrDcMimoDl = StringBuilder()
         val nrDcMimoUl = StringBuilder()
 
         componentsNr.populateCsvStringBuilders(
             nrBandBwScs,
             nrMimoDl,
-            nrUlBwMod,
+            nrUlBwScsMod,
             nrMimoUl,
             nrDlCC,
             nrUlCC,
@@ -84,13 +84,13 @@ data class ComboNrDc(
         componentsNrDc.populateCsvStringBuilders(
             nrDcBandBwScs,
             nrDcMimoDl,
-            nrDcUlBwMod,
+            nrDcUlBwScsMod,
             nrDcMimoUl,
             nrDcDlCC,
             nrDcUlCC,
             separator
         )
 
-        return "$compact$nrBandBwScs$nrDcBandBwScs$nrUlBwMod$nrDcUlBwMod$nrMimoDl$nrDcMimoDl$nrMimoUl$nrDcMimoUl$bcs"
+        return "$compact$nrBandBwScs$nrDcBandBwScs$nrUlBwScsMod$nrDcUlBwScsMod$nrMimoDl$nrDcMimoDl$nrMimoUl$nrDcMimoUl$bcs"
     }
 }

src/main/java/it/smartphonecombo/uecapabilityparser/util/IO.kt
@@ -160,6 +160,12 @@ object IO {
                 .append("NR UL")
                 .append(i)
                 .append(separator)
+                .append("NR UL BW")
+                .append(i)
+                .append(separator)
+                .append("NR UL SCS")
+                .append(i)
+                .append(separator)
                 .append("NR UL MOD")
                 .append(i)
                 .append(separator)
@@ -167,6 +173,12 @@ object IO {
         for (i in 1..nrDcUlCC) {
             header
                 .append("FR2 UL")
+                .append(i)
+                .append(separator)
+                .append("FR2 UL BW")
+                .append(i)
+                .append(separator)
+                .append("FR2 UL SCS")
                 .append(i)
                 .append(separator)
                 .append("FR2 UL MOD")

commit 1a6c87088f06a4331f80f1fc49df497a2d65287a

fix NR csv export
broken since 502a8e7

src/main/java/it/smartphonecombo/uecapabilityparser/extension/List.kt
@@ -77,5 +77,7 @@ internal fun List<ComponentNr>.populateCsvStringBuilders(
         IO.appendSeparator(separator, nrBandBwScs, nrBandBwScs, nrBandBwScs, nrMimoDl)
     }
 
-    repeat(ulCCs - ulCount) { IO.appendSeparator(separator, nrUlBwMod, nrUlBwMod, nrMimoUl) }
+    repeat(ulCCs - ulCount) {
+        IO.appendSeparator(separator, nrUlBwMod, nrUlBwMod, nrUlBwMod, nrUlBwMod, nrMimoUl)
+    }
 }