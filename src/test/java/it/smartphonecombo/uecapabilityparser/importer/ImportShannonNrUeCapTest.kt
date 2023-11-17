4385906cacaa4747ad0be9a000c63fc0c4b32514
handymenny <handymenny@outlook.com>
2023-11-17 16:47:54 +0100

    add tests for ImportShannonNrUeCap

---
+++ src/test/java/it/smartphonecombo/uecapabilityparser/importer/ImportShannonNrUeCapTest.kt
@@ -0,0 +1,52 @@
+package it.smartphonecombo.uecapabilityparser.importer
+
+import it.smartphonecombo.uecapabilityparser.model.Capabilities
+import java.io.File
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class ImportShannonNrUeCapTest {
+    private val path = "src/test/resources/shannon/"
+
+    private fun parse(inputFilename: String, oracleFilename: String) {
+        val filePath = "$path/input/$inputFilename"
+        val actual = ImportShannonNrUeCap.parse(File(filePath).readBytes())
+        val expected =
+            Json.decodeFromString<Capabilities>(
+                File("$path/oracleForImport/$oracleFilename").readText()
+            )
+
+        // Override dynamic properties
+        expected.parserVersion = actual.parserVersion
+        expected.timestamp = actual.timestamp
+        expected.id = actual.id
+
+        Assertions.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun parseEmpty() {
+        parse("empty.binarypb", "empty.json")
+    }
+
+    @Test
+    fun parseSub6() {
+        parse("sub6.binarypb", "sub6.json")
+    }
+
+    @Test
+    fun parseSub62() {
+        parse("sub6_2.binarypb", "sub6_2.json")
+    }
+
+    @Test
+    fun parseMmWave() {
+        parse("mmWave.binarypb", "mmWave.json")
+    }
+
+    @Test
+    fun parseMmWave2() {
+        parse("mmWave_2.binarypb", "mmWave_2.json")
+    }
+}
