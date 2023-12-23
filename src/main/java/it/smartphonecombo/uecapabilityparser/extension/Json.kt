6ee048791df59ecc86054897484f974c46edd289
handymenny <handymenny@outlook.com>
2023-12-23 16:28:27 +0100

    Json.decodeFromInputSource: Remove string specific condition

--- src/main/java/it/smartphonecombo/uecapabilityparser/extension/Json.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/extension/Json.kt
@@ -3,7 +3,6 @@ package it.smartphonecombo.uecapabilityparser.extension
 import io.javalin.config.SizeUnit
 import it.smartphonecombo.uecapabilityparser.io.GzipFileInputSource
 import it.smartphonecombo.uecapabilityparser.io.InputSource
-import it.smartphonecombo.uecapabilityparser.io.StringInputSource
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
@@ -15,12 +14,7 @@ internal fun Json.custom() = json
 
 @OptIn(ExperimentalSerializationApi::class)
 internal inline fun <reified T> Json.decodeFromInputSource(input: InputSource): T {
-    val stream =
-        when (input) {
-            is GzipFileInputSource -> true
-            is StringInputSource -> false
-            else -> input.size() > SizeUnit.MB.multiplier
-        }
+    val stream = input is GzipFileInputSource || input.size() > SizeUnit.MB.multiplier
 
     return if (stream) {
         input.inputStream().use { decodeFromStream<T>(it) }
