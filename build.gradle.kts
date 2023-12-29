cf455c17177c45e0f5810d505095f4d7d81bce5e
handymenny <handymenny@outlook.com>
2023-12-29 18:31:19 +0100

    update mts-asn1 to support tems logs

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ val tstypesImplementation: Configuration by
     configurations.getting { extendsFrom(configurations.implementation.get()) }
 
 dependencies {
-    val mtsAsn1Version = "b532fec"
+    val mtsAsn1Version = "7ee563f"
     val javalinVersion = "5.6.3"
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
