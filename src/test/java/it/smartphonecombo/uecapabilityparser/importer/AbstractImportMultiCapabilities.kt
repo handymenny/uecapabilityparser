ede7e4895bbfa590bfc819e3159aae3de4ad3f48
handymenny <handymenny@outlook.com>
2024-11-01 21:44:03 +0100

    tests: Introduce AbstractImportMultiCapabilities

---
+++ src/test/java/it/smartphonecombo/uecapabilityparser/importer/AbstractImportMultiCapabilities.kt
@@ -0,0 +1,47 @@
+package it.smartphonecombo.uecapabilityparser.importer
+
+import it.smartphonecombo.uecapabilityparser.extension.toInputSource
+import it.smartphonecombo.uecapabilityparser.importer.multi.ImportMultiCapabilities
+import it.smartphonecombo.uecapabilityparser.importer.multi.ImportScat
+import it.smartphonecombo.uecapabilityparser.model.Capabilities
+import it.smartphonecombo.uecapabilityparser.model.LogType
+import java.io.File
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Assertions
+
+abstract class AbstractImportMultiCapabilities(
+    private val parser: ImportMultiCapabilities,
+    private val path: String,
+    private val oracleSubDir: String = "oracle",
+) {
+
+    fun parse(inputFilename: String, oracleFilename: String, logType: LogType = LogType.INVALID) {
+        val filePath = "$path/input/$inputFilename"
+        val oraclePath = "$path/$oracleSubDir/$oracleFilename"
+
+        val input = File(filePath).toInputSource()
+        val multi =
+            if (parser !is ImportScat) {
+                parser.parse(input)
+            } else {
+                parser.parse(input, logType)
+            }
+
+        val actual = multi?.parsingList?.map { it.capabilities }!!
+
+        val expected = Json.decodeFromString<List<Capabilities>>(File(oraclePath).readText())
+
+        // Check size
+        Assertions.assertEquals(expected.size, actual.size)
+
+        // override dynamic properties
+        for (i in expected.indices) {
+            val capA = actual[i]
+            val capE = expected[i]
+
+            capA.getStringMetadata("processingTime")?.let { capE.setMetadata("processingTime", it) }
+        }
+
+        Assertions.assertEquals(expected, actual)
+    }
+}
--- src/test/java/it/smartphonecombo/uecapabilityparser/importer/ImportPcapTest.kt
+++ src/test/java/it/smartphonecombo/uecapabilityparser/importer/ImportPcapTest.kt
@@ -1,169 +1,112 @@
 package it.smartphonecombo.uecapabilityparser.importer
 
-import it.smartphonecombo.uecapabilityparser.extension.toInputSource
 import it.smartphonecombo.uecapabilityparser.importer.multi.ImportPcap
-import it.smartphonecombo.uecapabilityparser.model.Capabilities
-import java.io.File
-import kotlinx.serialization.json.Json
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
-internal class ImportPcapTest {
-    private val path = "src/test/resources/pcap/"
-
+internal class ImportPcapTest :
+    AbstractImportMultiCapabilities(ImportPcap, "src/test/resources/pcap/") {
     // Only utra
     @Test
     fun testPduUtra() {
-        testPcap(
-            "$path/input/exportedPduUtra.pcap",
-            "$path/oracle/exportedPduUtra-capabilities.json",
-        )
+        parse("exportedPduUtra.pcap", "exportedPduUtra-capabilities.json")
     }
 
     // One eutra
     @Test
     fun testPduEutra() {
-        testPcap(
-            "$path/input/exportedPduEutra.pcap",
-            "$path/oracle/exportedPduEutra-capabilities.json",
-        )
+        parse("exportedPduEutra.pcap", "exportedPduEutra-capabilities.json")
     }
 
     // different filters, one duplicate
     @Test
     fun testPduEutraDifferentFilters() {
-        testPcap(
-            "$path/input/exportedPduEutraDifferentFilters.pcap",
-            "$path/oracle/exportedPduEutraDifferentFilters-capabilities.json",
+        parse(
+            "exportedPduEutraDifferentFilters.pcap",
+            "exportedPduEutraDifferentFilters-capabilities.json",
         )
     }
 
     // nr rrc + lte rrc
     @Test
     fun testPDUNrRrcLteRrc() {
-        testPcap(
-            "$path/input/exportedPduNrRrcLteRrc.pcap",
-            "$path/oracle/exportedPduNrRrcLteRrc-capabilities.json",
-        )
+        parse("exportedPduNrRrcLteRrc.pcap", "exportedPduNrRrcLteRrc-capabilities.json")
     }
 
     // different MRDC capabilities all split
     @Test
     fun testPduMrdcFullSplit() {
-        testPcap(
-            "$path/input/exportedPduMrdcFullSplit.pcap",
-            "$path/oracle/exportedPduMrdcFullSplit-capabilities.json",
-        )
+        parse("exportedPduMrdcFullSplit.pcap", "exportedPduMrdcFullSplit-capabilities.json")
     }
 
     // different capabilities mixed split
     @Test
     fun testPduMrdcMixedSplit() {
-        testPcap(
-            "$path/input/exportedPduMrdcMixedSplit.pcap",
-            "$path/oracle/exportedPduMrdcMixedSplit-capabilities.json",
-        )
+        parse("exportedPduMrdcMixedSplit.pcap", "exportedPduMrdcMixedSplit-capabilities.json")
     }
 
     // SIM 1: 127.0.0.1, SIM 2: 127.0.0.2
     @Test
     fun testGsmTapDualSim() {
-        testPcap("$path/input/gsmTapDualSim.pcap", "$path/oracle/gsmTapDualSim-capabilities.json")
+        parse("gsmTapDualSim.pcap", "gsmTapDualSim-capabilities.json")
     }
 
     // SIM 1: 127.0.0.1, SIM 2: 127.0.0.1
     @Test
     fun testGsmTapDualSimSameIp() {
-        testPcap(
-            "$path/input/gsmTapDualSimSameIP.pcap",
-            "$path/oracle/gsmTapDualSimSameIP-capabilities.json",
-        )
+        parse("gsmTapDualSimSameIP.pcap", "gsmTapDualSimSameIP-capabilities.json")
     }
 
     // many capabilities
     @Test
     fun testGsmTapComplex() {
-        testPcap("$path/input/gsmTapComplex.pcap", "$path/oracle/gsmTapComplex-capabilities.json")
+        parse("gsmTapComplex.pcap", "gsmTapComplex-capabilities.json")
     }
 
     // eutra + nr/eutra-nr, no 0xB826
     @Test
     fun testGsmTapMrdcSemiSplit() {
-        testPcap(
-            "$path/input/gsmTapMrdcSemiSplit.pcap",
-            "$path/oracle/gsmTapMrdcSemiSplit-capabilities.json",
-        )
+        parse("gsmTapMrdcSemiSplit.pcap", "gsmTapMrdcSemiSplit-capabilities.json")
     }
 
     // shannon
     @Test
     fun testGsmTapShannon() {
-        testPcap("$path/input/gsmTapShannon.pcap", "$path/oracle/gsmTapShannon-capabilities.json")
+        parse("gsmTapShannon.pcap", "gsmTapShannon-capabilities.json")
     }
 
     // 0xB826 + spurious data
     @Test
     fun testGsmTapSpuriousData() {
-        testPcap(
-            "$path/input/gsmTapSpuriousData.pcap",
-            "$path/oracle/gsmTapSpuriousData-capabilities.json",
-        )
+        parse("gsmTapSpuriousData.pcap", "gsmTapSpuriousData-capabilities.json")
     }
 
     // NGAP radio cap + S1AP radio cap + spurious data
     @Test
     fun testSCTP() {
-        testPcap("$path/input/sctpS1apNgap.pcap", "$path/oracle/sctpS1apNgap-capabilities.json")
+        parse("sctpS1apNgap.pcap", "sctpS1apNgap-capabilities.json")
     }
 
     // GSMTAPv3 Draft/proposal
     @Test
     fun testGsmTapV3() {
-        testPcap("$path/input/gsmTapV3.pcap", "$path/oracle/gsmTapV3.json")
+        parse("gsmTapV3.pcap", "gsmTapV3.json")
     }
 
     // GSMTAPv3 Draft/proposal  NR ue capability segmented
     @Test
     fun testGsmTapV3NrRrcSegmented() {
-        testPcap(
-            "$path/input/gsmTapV3NrRrcSegmented.pcap",
-            "$path/oracle/gsmTapV3NrRrcSegmented.json",
-        )
+        parse("gsmTapV3NrRrcSegmented.pcap", "gsmTapV3NrRrcSegmented.json")
     }
 
     // GSMTAPv2 MRDC ue capability segmented
     @Test
     fun testGsmTapMrdcSegmented() {
-        testPcap("$path/input/gsmTapMrdcSegmented.pcap", "$path/oracle/gsmTapMrdcSegmented.json")
+        parse("gsmTapMrdcSegmented.pcap", "gsmTapMrdcSegmented.json")
     }
 
     // Exported PDU MRDC ue capability segmented
     @Test
     fun testPduSegmented() {
-        testPcap(
-            "$path/input/exportedPduMrdcSegmented.pcap",
-            "$path/oracle/exportedPduMrdcSegmented.json",
-        )
-    }
-
-    private fun testPcap(path: String, oracle: String) {
-        val multi = ImportPcap.parse(File(path).toInputSource())
-
-        val actual = multi?.parsingList?.map { it.capabilities }!!
-
-        val expected = Json.decodeFromString<List<Capabilities>>(File(oracle).readText())
-
-        // Check size
-        Assertions.assertEquals(expected.size, actual.size)
-
-        // override dynamic properties
-        for (i in expected.indices) {
-            val capA = actual[i]
-            val capE = expected[i]
-
-            capE.setMetadata("processingTime", capA.getStringMetadata("processingTime") ?: "")
-        }
-
-        Assertions.assertEquals(expected, actual)
+        parse("exportedPduMrdcSegmented.pcap", "exportedPduMrdcSegmented.json")
     }
 }
