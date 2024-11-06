14fddb0afe401c67b3595732b44d51fb02c5e7b7
handymenny <handymenny@outlook.com>
2024-11-06 20:14:33 +0100

    bwclass: introduce invalid bw class
    Update BwClass.kt

--- src/main/java/it/smartphonecombo/uecapabilityparser/model/BwClass.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/model/BwClass.kt
@@ -8,7 +8,8 @@ import kotlinx.serialization.Serializable
  *
  * Use [valueOf] to create an instance of this class
  *
- * BwClass of '0' has a special meaning, it's [NONE].
+ * BwClass of '0' has a special meaning, it's [NONE]. BwClass of '?' has a special meaning, it's
+ * [INVALID].
  */
 @Serializable
 @JvmInline
@@ -17,6 +18,7 @@ private constructor(@Serializable(with = BwClassSerializer::class) private val b
     Comparable<BwClass> {
     companion object {
         val NONE = BwClass('0')
+        val INVALID = BwClass('?')
 
         /**
          * Return a BwClass whose value is the first character (uppercase) of [s].
@@ -24,10 +26,9 @@ private constructor(@Serializable(with = BwClassSerializer::class) private val b
          * If [s] is empty or null return [NONE].
          */
         fun valueOf(s: String?): BwClass {
-            if (s.isNullOrEmpty()) {
-                return NONE
-            }
-            return BwClass(s.first().uppercaseChar())
+            val char = s?.firstOrNull()?.uppercaseChar() ?: '0'
+
+            return safeValueOf(char)
         }
 
         /**
@@ -36,10 +37,9 @@ private constructor(@Serializable(with = BwClassSerializer::class) private val b
          * If [i] = 0 return [NONE].
          */
         fun valueOf(i: Int): BwClass {
-            if (i == 0) {
-                return NONE
-            }
-            return BwClass((i + 0x40).toChar())
+            val char = if (i != 0) (i + 0x40).toChar() else '0'
+
+            return safeValueOf(char)
         }
 
         /**
@@ -48,10 +48,20 @@ private constructor(@Serializable(with = BwClassSerializer::class) private val b
          * If [i] = 6 return [NONE].
          */
         fun valueOfMtkIndex(i: Int): BwClass {
-            if (i == 6) {
-                return NONE
+            return when (i) {
+                in 0 until 6 -> BwClass((i + 0x41).toChar())
+                6 -> NONE
+                else -> INVALID
             }
-            return BwClass((i + 0x41).toChar())
+        }
+
+        /** Return BwClass(char) for A-Z, [NONE] for '0', [INVALID] otherwise. */
+        private fun safeValueOf(char: Char): BwClass {
+            return when (char) {
+                in 'A'..'Z' -> BwClass(char)
+                '0' -> NONE
+                else -> INVALID
+            }
         }
     }
 
