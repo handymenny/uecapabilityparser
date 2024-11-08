1efa9caa98dc0a725353efc7ba6bee085b08623c
handymenny <handymenny@outlook.com>
2024-11-08 18:45:06 +0100

    uecapability: clamp CC BW if it's above what is reported in channelbws

--- src/main/java/it/smartphonecombo/uecapabilityparser/importer/ImportCapabilitySharedHelper.kt
+++ src/main/java/it/smartphonecombo/uecapabilityparser/importer/ImportCapabilitySharedHelper.kt
@@ -7,6 +7,7 @@ import it.smartphonecombo.uecapabilityparser.model.EmptyBCS
 import it.smartphonecombo.uecapabilityparser.model.EmptyMimo
 import it.smartphonecombo.uecapabilityparser.model.LinkDirection
 import it.smartphonecombo.uecapabilityparser.model.Mimo
+import it.smartphonecombo.uecapabilityparser.model.band.BandNrDetails
 import it.smartphonecombo.uecapabilityparser.model.band.IBandDetails
 import it.smartphonecombo.uecapabilityparser.model.bandwidth.Bandwidth
 import it.smartphonecombo.uecapabilityparser.model.bandwidth.toBandwidth
@@ -64,11 +65,28 @@ internal fun mergeComponentAndFeaturePerCC(
         val componentNr = (component as ComponentNr).copy()
         val dlFeatureNr = dlFeature?.typedList<FeaturePerCCNr>()
         val ulFeatureNr = ulFeature?.typedList<FeaturePerCCNr>()
+        val bandNrDetails = bandDetails as? BandNrDetails
 
-        applyNrFeaturesPerCC(LinkDirection.DOWNLINK, componentNr, dlFeatureNr, bandDetails?.modDL)
-        applyNrFeaturesPerCC(LinkDirection.UPLINK, componentNr, ulFeatureNr, bandDetails?.modUL)
+        applyNrFeaturesPerCC(LinkDirection.DOWNLINK, componentNr, dlFeatureNr, bandNrDetails)
+        applyNrFeaturesPerCC(LinkDirection.UPLINK, componentNr, ulFeatureNr, bandNrDetails)
 
         return componentNr
+    }
+}
+
+private fun clampNrBw(maxBwFeatures: Int, maxBwBands: Int): Int {
+    // In channelBWs:
+    //   - for fr1 an ue can't report 90MHz support
+    //   - for fr2 an ue can't report >= 400MHz support
+    // FR1 100MHz is a bit special:
+    //   - when it's mandatory in rel15 it's added in BwsBitMap constructor
+    //   - when it's optional in rel15 it should be reported in channelBWs-v1590
+    //   - if neither of the above applies, it's assumed that 100MHz (and 90MHz) isn't supported
+    // So we skip the clamp for bw = 90MHz and bw >= 400MHz
+    return if (maxBwFeatures != 90 && maxBwFeatures < 400) {
+        minOf(maxBwFeatures, maxBwBands)
+    } else {
+        maxBwFeatures
     }
 }
 
@@ -76,7 +94,7 @@ private fun applyNrFeaturesPerCC(
     direction: LinkDirection,
     component: ComponentNr,
     feature: List<FeaturePerCCNr>?,
-    bandMod: Modulation?,
+    bandDetails: BandNrDetails?,
 ) {
     if (feature.isNullOrEmpty()) {
         setSdlSul(direction, component)
@@ -85,7 +103,7 @@ private fun applyNrFeaturesPerCC(
 
     val mimo: Mimo
     val bw: Bandwidth
-    var mod: Modulation
+    val mod: Modulation
 
     // These are "single" features and shared between DL and UL
     // So we take the current value into account
@@ -94,28 +112,40 @@ private fun applyNrFeaturesPerCC(
     component.channelBW90mhz =
         component.channelBW90mhz || feature.any(FeaturePerCCNr::channelBW90mhz)
 
+    val maxBwBand =
+        bandDetails
+            ?.bandwidths
+            ?.find { it.scs == scs }
+            ?.let {
+                if (direction == LinkDirection.DOWNLINK) {
+                    it.bwsDL
+                } else {
+                    it.bwsUL
+                }
+            }
+            ?.maxOrNull() ?: Int.MAX_VALUE
+
     if (feature.size > 1) {
         mimo = Mimo.from(feature.map { it.mimo.average().toInt() })
-        bw = Bandwidth.from(feature.map(FeaturePerCCNr::bw))
+        bw = Bandwidth.from(feature.map { clampNrBw(it.bw, maxBwBand) })
         mod = Modulation.from(feature.map(IFeaturePerCC::qam))
     } else {
         val firstFeature = feature.first()
         mimo = firstFeature.mimo
-        bw = firstFeature.bw.toBandwidth()
+        bw = clampNrBw(firstFeature.bw, maxBwBand).toBandwidth()
         mod = firstFeature.qam.toModulation()
     }
-
-    /* mod in bandNrDetails takes precedence, because modulation in NR features means something else (see TS 38 306) */
-    mod = bandMod ?: mod
 
     if (direction == LinkDirection.DOWNLINK) {
         component.mimoDL = mimo
         component.maxBandwidthDl = bw
-        component.modDL = mod
+        /* mod in bandNrDetails takes precedence, because modulation in NR features means something else (see TS 38 306) */
+        component.modDL = bandDetails?.modDL ?: mod
     } else {
         component.mimoUL = mimo
         component.maxBandwidthUl = bw
-        component.modUL = mod
+        /* mod in bandNrDetails takes precedence, because modulation in NR features means something else (see TS 38 306) */
+        component.modUL = bandDetails?.modUL ?: mod
     }
 }
 
